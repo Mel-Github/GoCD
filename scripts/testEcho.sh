d351a8ac3d1310a0f3f969dc99dd8341900bffce
Melvin <mel000vin@yahoo.com>
2019-02-08 12:51:51 +0800

    Update testEcho.sh

--- scripts/testEcho.sh
+++ scripts/testEcho.sh
@@ -5,3 +5,6 @@ echo Parameters $@
 echo Parameter 0 is $0
 echo Parameter 1 is $1
 echo Parameter 2 is $2
+echo Direct display GO_STAGE_COUNTER $GO_STAGE_COUNTER 
+echo Direct display GO_PIPELINE_NAME $GO_PIPELINE_NAME
+echo Direct display GO_JOB_NAME $GO_JOB_NAME
