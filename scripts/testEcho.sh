82093d7d3bcb2e4cf30ececd0c4112e90ffaeac7
Melvin <mel000vin@yahoo.com>
2019-02-14 23:20:38 +0800

    Update testEcho.sh

--- scripts/testEcho.sh
+++ scripts/testEcho.sh
@@ -11,6 +11,7 @@ echo Direct display GO_JOB_NAME $GO_JOB_NAME
 
 GO_SERVER_RESTURL=https://10.148.0.7:8154/go
 
-echo string is "curl -X POST $GO_SERVER_RESTURL/properties/$GO_PIPELINE_NAME/$GO_PIPELINE_COUNTER/$GO_STAGE_NAME/$GO_STAGE_COUNTER/$GO_JOB_NAME/ipaddress -k  -H 'Confirm: true' -H 'Postman-Token: 40c07e5b-a4de-4e55-89d8-1da5ca1349ec' -H 'cache-control: no-cache' -d 'value=123'"
+echo string is "curl -X POST$GO _SERVER_RESTURL/properties/$GO_PIPELINE_NAME/$GO_PIPELINE_COUNTER/$GO_STAGE_NAME/$GO_STAGE_COUNTER/$GO_JOB_NAME/ipaddress -k -H 'Authorization: Basic ZGVwbG95ZXI6ZGVwbEB5ZXItR29DRA==' -H 'Confirm: true' -H 'Postman-Token: 04fe747a-6352-4388-acb9-5e01492fc787' -H 'cache-control: no-cache' -d 'value=123'"
 
-curl -X POST $GO_SERVER_RESTURL/properties/$GO_PIPELINE_NAME/$GO_PIPELINE_COUNTER/$GO_STAGE_NAME/$GO_STAGE_COUNTER/$GO_JOB_NAME/ipaddress -k  -H 'Confirm: true' -H 'Postman-Token: 40c07e5b-a4de-4e55-89d8-1da5ca1349ec' -H 'cache-control: no-cache' -d 'value=12345'
+
+curl -X POST$GO _SERVER_RESTURL/properties/$GO_PIPELINE_NAME/$GO_PIPELINE_COUNTER/$GO_STAGE_NAME/$GO_STAGE_COUNTER/$GO_JOB_NAME/ipaddress -k -H 'Authorization: Basic ZGVwbG95ZXI6ZGVwbEB5ZXItR29DRA==' -H 'Confirm: true' -H 'Postman-Token: 04fe747a-6352-4388-acb9-5e01492fc787' -H 'cache-control: no-cache' -d 'value=123'
